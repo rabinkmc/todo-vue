a9e3dfe07616770f386f0476a53e1495ba3bad6a
Rabin Dhamala <rabin.dhamala@aayulogic.com>
2021-12-09 17:12:10 +0545

    rendering list of todo-items

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="add">
+		<h1>To-Do lists</h1>
+		<ul>
+			<li v-for="item in toDoItems" :key="item.id">
+				<to-do-item :id="item.id" :label="item.label" :done="item.done"></to-do-item>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from './components/ToDoItem.vue';
+import uniqueId from 'lodash.uniqueid'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		ToDoItem
+	},
+	data() {
+		return {
+			toDoItems: [
+				{id: uniqueId('todo-'), label: 'django signals', done: false},
+				{id: uniqueId('todo-'), label: 'authentication', done: true},
+				{id: uniqueId('todo-'), label: 'vue js-components', done: true},
+			]
+		}
+	}
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<input :id="id" type="checkbox" :checked="isDone" />
+		<label for="todo-item">{{label}}</label>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		label: {required: true, type: String},
+		done: {default: false, type: Boolean},
+		id: {required: true, type: String}
+	},
+	data() {
+		return {
+			isDone: this.done,
+		}
+	}
+};
+</script>
